657090bc604e13da1a06bc49cba2800b2aa681f6
EgorVakulenko <egor7619247@gmail.com>
2022-04-26 21:43:54 +0300

    New

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 BOT_TOKEN = "5151998417:AAGgZTJmI3cS0VeTv7erac20ssTC5ZrnWyo"
-APP_URL = "https://iqokbot.herokuapp.com/" + BOT_TOKEN GGG
+APP_URL = "https://iqokbot.herokuapp.com/" + BOT_TOKEN
 DB_URI = "postgres://puituzgmngqiwh:9b4f079c0766513bd92150923a63bb17c1d3505460b059396fcb8d0599757aa2@ec2-54-78-211-131.eu-west-1.compute.amazonaws.com:5432/dcrqes2g6amhmi"
